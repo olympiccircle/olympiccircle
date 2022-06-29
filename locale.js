d69fce25ee73d02dc41208f7491072a4420d96ec
mykhailivchenko <mivchenko@exadel.com>
2022-06-29 08:41:18 +0300

    Refactoring

---
+++ locale.js
@@ -0,0 +1,149 @@
+export var language = {
+    
+    
+    
+    eng: {
+        
+        header_about_us:
+        "About us ",
+        header_donations:
+        "Donate",
+        header_contacts:
+        "Contacts",
+        goal_title:
+        "Help Mykolaiv now!",
+
+        ///Quadro Section
+        goal_quadro_title:
+        "Головна мета:",
+        goal_quadro_description:
+        "",
+        goal_quadro_cost:
+        "",
+
+        who_we_are_title:
+        "",
+        who_we_are_description:
+        "",
+
+        experience_title:
+        "",
+        experience_description:
+        "",
+
+        we_lov_mykolaiv_title:
+        "",
+        we_lov_mykolaiv_description:
+        "",
+
+        we_help_title:
+        "",
+        we_help_description:
+        "",
+        we_help_text_img1:
+        "",
+        we_help_text_img1:
+        "",
+        we_help_text_img3:
+        ""
+    },
+    
+//===============================UA section=======================================>
+    
+    ua: {
+        header_about_us:
+        "Про нас",
+        header_donations:
+        "Донат",
+        header_contacts:
+        "Контакти",
+        goal_title:
+        "",
+
+        ///Quadro Section
+        goal_quadro_title:
+        "",
+        goal_quadro_description:
+        "",
+        goal_quadro_cost:
+        "",
+
+        who_we_are_title:
+        "",
+        who_we_are_description:
+        "",
+
+        experience_title:
+        "",
+        experience_description:
+        "",
+
+        we_lov_mykolaiv_title:
+        "",
+        we_lov_mykolaiv_description:
+        "",
+        we_help_title:
+        "",
+        we_help_description:
+        "",
+        we_help_text_img1:
+        "",
+        we_help_text_img1:
+        "",
+        we_help_text_img3:
+        ""
+
+    },
+
+
+//=============================== 🐷 RU section=======================================>
+    
+    ru: {
+        header_about_us:
+        "О нас",
+        header_donations:
+        "Донат",
+        header_contacts:
+        "Контакты",
+        goal_title:
+        "Help Mykolaiv now!",
+
+        ///Quadro Section
+        goal_quadro_title:
+        "Головна мета:",
+        goal_quadro_description:
+        "",
+        goal_quadro_cost:
+        "",
+
+        who_we_are_title:
+        "",
+        who_we_are_description:
+        "",
+
+        experience_title:
+        "",
+        experience_description:
+        "",
+
+        we_lov_mykolaiv_title:
+        "",
+        we_lov_mykolaiv_description:
+        "",
+
+        we_help_title:
+        "",
+        we_help_description:
+        "",
+        we_help_text_img1:
+        "",
+        we_help_text_img1:
+        "",
+        we_help_text_img3:
+        ""
+
+    },
+  
+
+
+};
